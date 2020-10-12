949ccd8ef057d185f2436851e3ad01f2d40a1550
Anshuman Gupta <anshuman844@gmail.com>
2020-10-12 21:54:15 +0530

    feat: Add palettes module

---
+++ lib/palettes.py
@@ -0,0 +1,28 @@
+import numpy as np
+from collections import Counter
+
+def palette(clusters):
+    width=300
+    palette = np.zeros((50, width, 3), np.uint8)
+    steps = width/clusters.cluster_centers_.shape[0]
+    for idx, centers in enumerate(clusters.cluster_centers_): 
+        palette[:, int(idx*steps):(int((idx+1)*steps)), :] = centers
+    return palette
+
+def palette_perc(k_cluster):
+    width = 300
+    palette = np.zeros((50, width, 3), np.uint8)
+    n_pixels = len(k_cluster.labels_)
+    counter = Counter(k_cluster.labels_) # count how many pixels per cluster
+    perc = {}
+    for i in counter:
+        perc[i] = np.round(counter[i]/n_pixels, 2)
+    perc = dict(sorted(perc.items()))
+    #for logging purposes
+    #print(perc)
+    #print(k_cluster.cluster_centers_)
+    step = 0
+    for idx, centers in enumerate(k_cluster.cluster_centers_): 
+        palette[:, step:int(step + perc[idx]*width+1), :] = centers
+        step += int(perc[idx]*width+1)
+    return palette
